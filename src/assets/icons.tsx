5a3b001f35b398aacccd08349154b713e628b046
Arnanta Chatterjee <arnantachatterjee000@gmail.com>
2025-07-03 01:02:34 +0530

    Init commit - Navbar homepage design in progress

---
+++ src/assets/icons.tsx
@@ -0,0 +1,273 @@
+export const ChevronDown = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M480-360 280-560h400L480-360Z" />
+  </svg>
+);
+
+export const ChevronUp = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="m280-400 200-200 200 200H280Z" />
+  </svg>
+);
+
+export const NewFile = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M240-160q-33 0-56.5-23.5T160-240v-480q0-33 23.5-56.5T240-800h320l240 240v320q0 33-23.5 56.5T720-160H240Zm280-400v-200H240v480h480v-280H520ZM240-800v200-200 480-480Z" />
+  </svg>
+);
+
+export const AddFile = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M440-440H320v-80h120v-120h80v120h120v80H520v120h-80v-120ZM240-80q-33 0-56.5-23.5T160-160v-640q0-33 23.5-56.5T240-880h320l240 240v480q0 33-23.5 56.5T720-80H240Zm280-520v-200H240v640h480v-440H520ZM240-800v200-200 640-640Z" />
+  </svg>
+);
+
+export const Folder = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h240l80 80h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Z" />
+  </svg>
+);
+
+export const Refresh = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z" />
+  </svg>
+);
+
+export const CollapseAll = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="m280-200-56-56 224-224 224 224-56 56-168-168-168 168Zm0-240-56-56 224-224 224 224-56 56-168-168-168 168Z" />
+  </svg>
+);
+
+export const ExplorerIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M240-160q-33 0-56.5-23.5T160-240v-480q0-33 23.5-56.5T240-800h240l80 80h320q33 0 56.5 23.5T960-640v400q0 33-23.5 56.5T880-160H240Zm0-80h480v-400H452l-80-80H240v480Zm240-120 126-160v-120H360v120l124 160h-4Zm-240 40v-480 480Z" />
+  </svg>
+);
+
+export const CloseIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+  </svg>
+);
+
+export const ChevronRight = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z" />
+  </svg>
+);
+
+export const DeleteIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#14B8A6"
+  >
+    <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
+  </svg>
+);
+
+export const AddIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z" />
+  </svg>
+);
+
+export const MenuIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="32px"
+    viewBox="0 -960 960 960"
+    width="32px"
+    fill="#e3e3e3"
+  >
+    <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+  </svg>
+);
+
+export const AvatarIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="black"
+  >
+    <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z" />
+  </svg>
+);
+
+export const DarkMode = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#e3e3e3"
+  >
+    <path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" />
+  </svg>
+);
+
+export const EditIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="#14B8A6"
+  >
+    <path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z" />
+  </svg>
+);
+
+export const ChevronDownIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="black"
+  >
+    <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+  </svg>
+);
+export const ChevronUpIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="black"
+  >
+    <path d="M480-528 296-344l-56-56 240-240 240 240-56 56-184-184Z" />
+  </svg>
+);
+
+export const Cart = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="black"
+  >
+    <path d="M292-120q-38 0-65-27.5T200-213v-371l-73-176H40v-80h141l66 160h591q23 0 35 19t1 39L760-399q51 8 85.5 47t34.5 92q0 58-40.5 99T741-120q-59 0-99.5-41T601-260q0-20 5-37t14-33l-131-12-120 180q-13 20-33.5 31T292-120Zm382-285 99-195H280l50 120q8 20 25.5 33.5T396-431l278 26ZM293-201q2 0 9-5l97-144q-49-5-77-23.5T280-412v200q0 5 4 8t9 3Zm447 1q26 0 43-17.5t17-42.5q0-26-17-43t-43-17q-25 0-42.5 17T680-260q0 25 17.5 42.5T740-200Zm-66-205-278-26 278 26Z" />
+  </svg>
+);
+
+export const Favorite = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="black"
+  >
+    <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" />
+  </svg>
+);
+
+export const Search = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24px"
+    viewBox="0 -960 960 960"
+    width="24px"
+    fill="black"
+  >
+    <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
+  </svg>
+);
+// Export all icons as named exports
+export default {
+  ChevronDown,
+  ChevronUp,
+  NewFile,
+  AddFile,
+  Folder,
+  Refresh,
+  CollapseAll,
+  ExplorerIcon,
+  CloseIcon,
+  ChevronRight,
+  DeleteIcon,
+  AddIcon,
+  MenuIcon,
+  AvatarIcon,
+  DarkMode,
+  EditIcon,
+  ChevronDownIcon,
+  Cart,
+  Favorite,
+  Search,
+};
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import style from "./styles/HomePage.module.css";
+import { Link } from "react-router-dom";
+import { Cart, Favorite, Search } from "../assets/icons";
+const HomePage = () => {
+  //HomePage - design
+  //navbar in the top
+  //Then the page is divided into two sections - a left side bar - taking 30% width and the main feature products section - 70%
+  // Let's design the navbar first and then will divide the remaining page into main section
+  return (
+    <div className={style.container}>
+      {/*Navbar containing div - everything of navbar- separate it out to a different  */}
+      <div className={style.navContainer}>
+        <div className={style.section}>
+          <Link to="/" className={`${style.navLink} ${style.companyName}`}>
+            Flipazon
+          </Link>
+        </div>
+
+        <div className={style.section}>
+          <nav className={style.navbar}>
+            <Link to="/home" className={style.navLink}>
+              Home
+            </Link>
+            <Link to="/contact" className={style.navLink}>
+              Contact
+            </Link>
+            <Link to="/about" className={style.navLink}>
+              About
+            </Link>
+            <Link to="/signup" className={style.navLink}>
+              Signup
+            </Link>
+          </nav>
+        </div>
+        <div className={style.rightMostSection}>
+          <div className={style.inputWithIcon}>
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              name="serachProducts"
+            />
+            <Search />
+          </div>
+          <div className={style.otherIcons}>
+            <Favorite />
+            <Cart />
+          </div>
+        </div>
+      </div>
+      {/* Div containing the content section of the page - will have the whole product listing */}
+      <div className={style.contentSection}></div>
+    </div>
+  );
+};
+
+export default HomePage;
